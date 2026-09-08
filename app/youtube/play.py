64dd940738af55298e4fa0f6848b263d528ceea1
fadiy <139784128+fadiymusthafa@users.noreply.github.com>
2026-09-08 14:27:06 +0530

    Update play.py

--- app/youtube/play.py
+++ app/youtube/play.py
@@ -49,4 +49,19 @@ def create_youtube_url(command):
         pattern,
         text
     )
-    
+    if match:
+
+      query = match.group(1)
+      break
+  query = query.strip()
+
+  video_id = get_vid(query)
+
+  if not video_id:
+    return None
+  return(
+    "https://www.youtube.com/embed/"
+    + video_id
+    + "?autoplay=1&mute=0"
+  )
+  
